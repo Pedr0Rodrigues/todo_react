62d78743cbedf1329f10d6374ca0f077381157e6
Pedro Rodrigues <pedroaugusto2610@poli.ufrj.br>
2023-08-25 13:55:39 -0300

    search feature task complete.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,54 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import TaskItem from "../components/TaskItem";
 
 export default function TodoList() {
-  const initialTasks = [1, 2, 3, 4, 5, 6].map((i) => ({
-    id: i,
-    title: `Task ${i}`,
-    completed: i % 2 === 0,
-  }));
+  const initialTasks = [
+    {
+      id: 1,
+      title: 'Acordar',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Entregar',
+      completed: true,
+    },
+    {
+      id: 3,
+      title: 'Apresentar',
+      completed: false,
+    },   
+    {
+      id: 4,
+      title: 'Commitar',
+      completed: false,
+    },    
+    {
+      id: 5,
+      title: 'Fazer o PR',
+      completed: false,
+    },
+
+
+  ];
 
   const [tasks, setTasks] = useState(initialTasks);
-
   const [newTitleTask, setNewTitleTask] = useState("");
+  const [searchInput, setSearchInput] = useState("");
 
+
+  const handleEnterKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleAddNewTask();
+    }
+  };
+  
   const handleNewTitleChange = (e) => {
     setNewTitleTask(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    setSearchInput(e.target.value);
   };
 
   const handleAddNewTask = () => {
@@ -24,18 +59,39 @@ export default function TodoList() {
     };
 
     setTasks([...tasks, newTask]);
-
     setNewTitleTask("");
   };
 
   const handleUpdateTask = (updatedTask) => {
-    setTasks([...tasks.filter((t) => t.id !== updatedTask.id), updatedTask]);
+    setTasks((prevTasks) =>
+      prevTasks.map((t) => (t.id === updatedTask.id ? updatedTask : t))
+    );
   };
 
   const handleDeleteTask = (taskId) => {
-    setTasks([...tasks.filter((task) => task.id !== taskId)]);
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
   };
 
+  const searchList = () => {
+    const filteredTasks = tasks.filter(
+      (task) =>
+        task.title.toLowerCase().includes(searchInput.toLowerCase()) &&
+        task.completed === false
+    );
+  
+    return (
+      <div>
+        {filteredTasks.map((task) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            onUpdateTask={handleUpdateTask}
+            onDeleteTask={handleDeleteTask}
+          />
+        ))}
+      </div>
+    );
+  };
   return (
     <div className="container">
       <p>
@@ -46,21 +102,20 @@ export default function TodoList() {
           type="text"
           value={newTitleTask}
           onChange={handleNewTitleChange}
+          onKeyUp={handleEnterKeyPress} 
         />
+        
         <button onClick={handleAddNewTask}>Add</button>
       </p>
       <h3>Todo</h3>
       <ul id="incomplete-tasks">
-        {tasks
-          .filter((task) => !task.completed)
-          .map((task) => (
-            <TaskItem
-              key={task.id}
-              task={task}
-              onUpdateTask={handleUpdateTask}
-              onDeleteTask={handleDeleteTask}
-            />
-          ))}
+      <input
+          id = "seach-task"
+          type="search"
+          placeholder="Search here your incomplete tasks!"
+          onChange={handleSearch}
+          value={searchInput}/>
+          {searchList()}
       </ul>
       <h3>Completed</h3>
       <ul id="completed-tasks">
@@ -77,4 +132,4 @@ export default function TodoList() {
       </ul>
     </div>
   );
-}
+};
